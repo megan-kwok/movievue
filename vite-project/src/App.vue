4fcbc203605060082ff06d6990f37d9034429911
megan-kwok <miraaskra@gmail.com>
2022-11-20 18:54:20 -0500

    Made a getmovies component

---
+++ vite-project/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import MovieSelect from "./components/GetMovies.vue";
+</script>
+
+<template>
+    <div id="MovieSelect">
+        <select v-model="Movies" id="Movies">
+            <option value="483455">Dead Apple</option>
+            <option value="198663">The Maze Runner</option>
+            <option value="575813">Better days</option>
+            <option value="76826">Grave of the Fireflies</option>
+            <option value="851644">20th Century Girl</option>
+            <option value="88751">Journey to the Center of the Earth</option>
+            <option value="730154">Your Eyes Tell</option>
+            <option value="433422">Fairy Tail: Dragon Cry</option>
+            <option value="329">Jurassic Park I</option>
+            <option value="497582">Enola Holmes II</option>
+        </select>
+        <button v-on:click="getMovies">Get</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-project/src/components/GetMovies.vue
@@ -0,0 +1,33 @@
+<script setup>
+import axios from 'axios'
+const props = defineProps(["api_key", "id"]);
+const response = (
+  await axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
+    params: {
+      id: props.id,
+      api_key: "26ca7d300d9e397095fa7e1435f5eb3d",
+    },
+  })
+).data;
+</script>
+
+<template>
+    <div class="movies">
+        <div v-for='result in results' :key='result.id'>
+        <h1>{{result.title}}</h1>
+        <p>{{result.overview}}</p>
+        <p>{{result.runtime}}</p>
+        <p>{{result.release_date}}</p>
+        <p>{{result.popularity}}</p>
+        <p>{{result.budget}}</p>
+        <p>{{result.vote_average}}</p>
+        <p>{{result.original_language}}</p>
+        <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" width='100px'>
+        <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.backdrop_path" width='100px'>
+    </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
