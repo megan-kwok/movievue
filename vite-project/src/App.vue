d7df4f99d8cd5e319229e27c713f4a25f3215d3b
[megan <miraaskra@gmail.com>
2022-11-23 21:49:06 -0500

    Edited movie component js

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,25 +1,25 @@
 <script setup>
-import MovieSelect from "./components/GetMovies.vue";
+import GetMovies from "./components/GetMovies.vue";
+
 </script>
 
 <template>
-    <div id="MovieSelect">
-        <select v-model="Movies" id="Movies">
-            <option value="483455">Dead Apple</option>
-            <option value="198663">The Maze Runner</option>
-            <option value="575813">Better days</option>
-            <option value="76826">Grave of the Fireflies</option>
-            <option value="851644">20th Century Girl</option>
-            <option value="88751">Journey to the Center of the Earth</option>
-            <option value="730154">Your Eyes Tell</option>
-            <option value="433422">Fairy Tail: Dragon Cry</option>
-            <option value="329">Jurassic Park I</option>
-            <option value="497582">Enola Holmes II</option>
-        </select>
-        <button v-on:click="getMovies">Get</button>
+  <div class="app-container">
+    <Suspense>
+      <GetMovies />
+    </Suspense>
     </div>
 </template>
 
 <style scoped>
+* {
+  margin: 0px;
+  padding: 0px;
+}
 
+.app-container {
+  display: grid;
+  grid-template-columns: repeat (10 , 1fr);
+  grid-template-rows: repeat (10 , 1fr)
+}
 </style>
--- vite-project/src/components/GetMovies.vue
+++ vite-project/src/components/GetMovies.vue
@@ -1,33 +1,55 @@
 <script setup>
-import axios from 'axios'
-const props = defineProps(["api_key", "id"]);
-const response = (
+import { ref } from "vue"
+import axios from "axios"
+ 
+const movies = ref("");
+const response = ref(null);
+ 
+const getMovies = async () => {
+   response.value = (
   await axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
     params: {
-      id: props.id,
       api_key: "26ca7d300d9e397095fa7e1435f5eb3d",
     },
   })
 ).data;
+}
+axios(getMovies)
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
 </script>
-
+ 
 <template>
-    <div class="movies">
-        <div v-for='result in results' :key='result.id'>
-        <h1>{{result.title}}</h1>
-        <p>{{result.overview}}</p>
-        <p>{{result.runtime}}</p>
-        <p>{{result.release_date}}</p>
-        <p>{{result.popularity}}</p>
-        <p>{{result.budget}}</p>
-        <p>{{result.vote_average}}</p>
-        <p>{{result.original_language}}</p>
-        <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" width='100px'>
-        <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.backdrop_path" width='100px'>
-    </div>
-    </div>
+      <select v-model="movies" id="movies">
+            <option value="483455">Dead Apple</option>
+            <option value="198663">The Maze Runner</option>
+            <option value="575813">Better days</option>
+            <option value="76826">Grave of the Fireflies</option>
+            <option value="851644">20th Century Girl</option>
+            <option value="88751">Journey to the Center of the Earth</option>
+            <option value="730154">Your Eyes Tell</option>
+            <option value="433422">Fairy Tail: Dragon Cry</option>
+            <option value="329">Jurassic Park I</option>
+            <option value="497582">Enola Holmes II</option>
+        </select>
+        <button @click="getMovies">Get</button>
+        <!-- <div v-if="response" class="movies-container">
+        <h1>{{ response.movies.title }}</h1>
+        <p>{{ response.overview }}</p>
+        <p>{{ response.runtime }}</p>
+        <p>{{ response.release_date }}</p>
+        <p>{{ response.popularity }}</p>
+        <p>{{ response.budget }}</p>
+        <p>{{ response.vote_average }}</p>
+        <p>{{ response.original_language }}</p>
+        <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +     response.poster_path" width='100px'>
+        <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +     response.backdrop_path" width='100px'> -->
+    <!-- </div> -->
 </template>
-
+ 
 <style scoped>
-
+div p {
+  text-align: center;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
 </style>
